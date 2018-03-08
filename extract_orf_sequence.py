dfd8874b4d639b9eb9f0b92c47ed692bcff9b664
AlexWanghaoming <568019240@qq.com>
2018-03-08 09:30:40 +0800

    Update extract_orf_sequence.py

--- extract_orf_sequence.py
+++ extract_orf_sequence.py
@@ -6,16 +6,17 @@ import re
 import time
 genome=open('/home/malab19/1.9TB_Volume_mounted/whm_data/tair10/tair10.fa')
 orf_stats=pandas.read_csv('/home/malab19/1.9TB_Volume_mounted/whm_data/tair10/orf_stats_new2.csv')
-coords=orf_stats["orf_coords"]
+# coords=orf_stats["orf_coords"]这里的coords对象是series对象
 
-chr=orf_stats.iloc[:,[1]]
+chr=orf_stats.iloc[:,[1]]#chr对象是dataframe对象，可以用for循环
 list1=[]
 for i in chr["orf_coords"]:
     chromsome=str(i).split(':')[0]
     list1.append(chromsome)
-all_chr = set(list1)
+all_chr = set(list1)#去除重复
 # def get_position():
-# position = {'chr1':[],'chr2':[],'chr3':[],'chr4':[],'chr5':[],'chrMt':[],'chrPt':[]}
+# position = {'chr1':[],'chr2':[],'chr3':[],'chr4':[],'chr5':[],'chrMt':[],'chrPt':[]}#建立一个字典，每个染色体对应一个大列表 
+                                                                               #列表中包括该染色体上所有orf的起始和终止位置[start,stop]
 # for k in all_chr:
 #     for j in coords:
 #         split_coords=re.split(':|-',j)
@@ -27,8 +28,8 @@ all_chr = set(list1)
 #             position[k].append(temp1)
 #             temp1=[]
 outf=open('/home/malab19/1.9TB_Volume_mounted/whm_data/tair10/tair10_orf.fa','r+')
-sequence={'chr1':'','chr2':'','chr3':'','chr4':'','chr5':'','chrMt':'','chrPt':''}
-title_list=[1514792]
+sequence={'chr1':'','chr2':'','chr3':'','chr4':'','chr5':'','chrMt':'','chrPt':''}#建一个字典，存入fasta文件中每条染色体下的所有序列
+title_list=[1514792]#这是拟南芥基因组fasta文件的行数
 count=0
 genome_list=genome.readlines()
 for index,item in enumerate(genome_list):
@@ -37,10 +38,11 @@ for index,item in enumerate(genome_list):
             count+=1
             title_list.append(index)
             title_list.sort()
-            temp2=''.join(genome_list[index+1:title_list[count]]).replace('\n','')
+            temp2=''.join(genome_list[index+1:title_list[count]]).replace('\n','')#从一个'>'到下一个‘>’之间的序列存入temp2
             sequence[i]=sequence[i]+temp2
             temp2=[]
 # print(sequence['chr1'][3759:5630])
+#对于反义链进行反向互补配对
 def reverse_complement(chr,start,stop):
     ntComlement = {'A':'T','C':'G','T':'A','G':'C'}
     seq=''
@@ -50,12 +52,12 @@ def reverse_complement(chr,start,stop):
     return ''.join(revSeq)
 
 print(orf_stats.iloc[0,2].split(':')[0])
-col_num = int(orf_stats.describe().iloc[0,0])
+col_num = int(orf_stats.describe().iloc[0,0])#这个方法用来获得dataframe的行数
 for line in range(col_num):
-    if orf_stats.iloc[line,1].split(':')[2] == '+':
+    if orf_stats.iloc[line,1].split(':')[2] == '+':#正义链
         outf.write('>{0}_{1}_{2}\n{3}\n'.format(orf_stats.iloc[line,1].split(':')[0],orf_stats.iloc[line,0].split(':')[1],orf_stats.iloc[line,4],\
         sequence[orf_stats.iloc[line,1].split(':')[0]][int(re.split(':|-',orf_stats.iloc[line,1])[1])-1:int(re.split(':|-',orf_stats.iloc[line,1])[2])]))
-    else:
+    else:#反义链
         outf.write('>{0}_{1}_{2}\n{3}\n'.format(orf_stats.iloc[line,1].split(':')[0],orf_stats.iloc[line,0].split(':')[1],orf_stats.iloc[line,4],\
         reverse_complement(orf_stats.iloc[line,1].split(':')[0],int(re.split(':|-',orf_stats.iloc[line,1])[1]),int(re.split(':|-',orf_stats.iloc[line,1])[2]))))
 
