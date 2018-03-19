31e54eafea147477fa6328995aa2ac565fd43868
AlexWanghaoming <568019240@qq.com>
2018-03-19 21:16:37 +0800

    Create mutiprocess.py

---
+++ mutiprocess.py
@@ -0,0 +1,13 @@
+
+import multiprocessing
+import subprocess
+print(multiprocessing.cpu_count())
+for i in range(2,4):
+    def worker_i():
+        subprocess.Popen("./lastz --format=maf --ambiguous=iupac --notransition /home/malab19/1.9TB_Volume_mounted/whm_data/AGP4.35/shell_result/{0}_result\
+         /home/malab19/1.9TB_Volume_mounted/whm_data/tair10/tair10.fa > /home/malab19/1.9TB_Volume_mounted/whm_data/lastz_result/lastz_result_{1}".format(i,i),shell=True)
+         
+    creatVar = locals()
+    creatVar['p' + str(i)] = multiprocessing.Process(target=worker_i)
+    s=locals()['p'+str(i)]
+    s.start()
